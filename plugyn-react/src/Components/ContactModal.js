b9d20159fa76aee24a96e7b5708676717bba88ad
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2020-02-15 19:40:06 +0200

    Bright logos

--- plugyn-react/src/Components/ContactModal.js
+++ plugyn-react/src/Components/ContactModal.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Modal, Button, Row, Col } from 'react-bootstrap'
 
-import DiscordLogo from "../discLogo.png"
-import SkypeLogo from "../skypeLogo.png"
+import DiscordLogo from "../discLogoBright.png"
+import SkypeLogo from "../skypeLogoBright.png"
 
 
 class ContactModal extends React.Component {
