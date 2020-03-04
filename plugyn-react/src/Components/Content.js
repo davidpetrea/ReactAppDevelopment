247069e750bcfde3acc14f4a746a5d0495d29ce0
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2020-03-04 15:03:50 +0200

    Added server text

--- plugyn-react/src/Components/Content.js
+++ plugyn-react/src/Components/Content.js
@@ -33,20 +33,20 @@ class Content extends React.Component{
                         </div>
                         <div className="container servers">
                             <div className="row">
-                                <Server image={backgroundEkstasia} serverName="Ekstasia" price="3.8kkk Yang = 10 €" serverDescription="Enjoy one of the most popular Private Servers by getting the best equipment out there! "/>
-                                <Server image={backgroundCelestial} serverName="Celestial World II" price="50k Lagis = 10 €" serverDescription="Grab an advantage on this unique Metin2 experience with entirely new systems and mechanics!"/>
+                                <Server image={backgroundEkstasia} serverName="Origins II" price="25kkk Yang = 10 €" serverDescription="With over 2000 players online, Origins2 has been a stable server for the past 8 months, enjoyable for those of you who are looking for a PvM – medium experience."/>
+                                <Server image={backgroundCelestial} serverName="Celestial World II" price="50k Lagis = 10 €" serverDescription="This 6-year old new-school metin2 is still kicking. One of the most unique-rich servers on the market, with mechanics similar to those in Diablo and Path of Exile, is waiting for you."/>
                             </div>
                             <div  className="row">
-                                <Server image={backgroundMenor} serverName="Age of Menor" price="300kk Yang = 10 €" serverDescription="Grab an advantage on this unique Metin2 experience with entirely new systems and mechanics!"/>
-                                <Server image={backgroundAeldra} serverName="Nayah II" price="50kkk Yang = 10 €" serverDescription="Get stronger today on the newest Metin2 Private Server available!"/>
+                                <Server image={backgroundMenor} serverName="Age of Menor" price="800kk Yang = 10 €" serverDescription="There’s nothing on the market quite like Age of Menor. This International server brings back the old-school vibes that made you fall in love with Metin2 in the first place. Feeling nostalgic? Give this highly-populated server a try."/>
+                                <Server image={backgroundAeldra} serverName="Nayah II" price="100kkk Yang = 10 €" serverDescription="Nayah2 brings a balanced PvP experience to the Metin2 scene, offering many battlegrounds as well as some PvM content along the way."/>
                             </div>
                             <div className="row">
-                                <Server image={backgroundAeldra} serverName="Nexus II" price="300kk Yang = 10 €" serverDescription="Grab an advantage on this unique Metin2 experience with entirely new systems and mechanics!"/>
-                                <Server image={background5} serverName="Evion" price="1.2kkk Yang = 10 €" serverDescription="Get stronger today on the newest Metin2 Private Server available!"/>
+                                <Server image={backgroundAeldra} serverName="Nexus II" price="1.5kkk Yang = 10 €" serverDescription="The definition of a PvM experience, this server brings a great deal of content to the table. An ever-growing community that actually makes its voice heard awaits for you!"/>
+                                <Server image={background5} serverName="Evion II" price="1.4kkk Yang = 10 €" serverDescription="Beginner-friendly and International, Evion2 is sure to please those of you who are looking for an adventure. Grab your friends and jump in this promising PvM experience together."/>
                             </div>
                             <div className="row">
-                                <Server image={backgroundMenor} serverName="Lysium" price="300kk Yang = 10 €" serverDescription="Grab an advantage on this unique Metin2 experience with entirely new systems and mechanics!"/>
-                                <Server image={backgroundCelestial} serverName="Someya" price="1.2kkk Yang = 10 €" serverDescription="Get stronger today on the newest Metin2 Private Server available!"/>
+                                <Server image={backgroundMenor} serverName="Rubinum DE" price="6 won/600kk = 10 €" serverDescription="The latest addition to the official DE metin2 server-list will surely offer you a great challenge with a fresh old-school vibe."/>
+                                <Server image={backgroundCelestial} serverName="Genesis DE" price="10 won/1kkk = 10 €" serverDescription="As part of the most populated official server, the Genesis community will make sure you have a great time and enjoy the experience."/>
                             </div>
                             
                         </div>
