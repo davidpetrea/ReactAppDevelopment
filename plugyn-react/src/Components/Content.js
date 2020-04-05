dd67cc078ccdbe0e42585cda9cc31511f3a13f92
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2020-04-05 15:19:12 +0300

    change price/ratings

--- plugyn-react/src/Components/Content.js
+++ plugyn-react/src/Components/Content.js
@@ -9,6 +9,8 @@ import background5 from "../metin2wp5edit.jpg"
 import background6 from "../metin2wp6edit.png"
 import background7 from "../metin2wp7edit.png"
 import background8 from "../metin2wp8edit.png"
+import background9 from "../metin2wp9edit.png"
+import background10 from "../metin2wp10edit.png"
 
 class Content extends React.Component{
     render() {
@@ -40,16 +42,20 @@ class Content extends React.Component{
                                 <Server image={backgroundCelestial} serverName="Celestial World II" price="50k Lagis = 10 €" serverDescription="This 6-year old new-school metin2 is still kicking. One of the most unique-rich servers on the market, with mechanics similar to those in Diablo and Path of Exile, is waiting for you."/>
                             </div>
                             <div  className="row">
-                                <Server image={backgroundMenor} serverName="Age of Menor" price="1kkk Yang = 10 €" serverDescription="There’s nothing on the market quite like Age of Menor. This International server brings back the old-school vibes that made you fall in love with Metin2 in the first place. Feeling nostalgic? Give this highly-populated server a try."/>
-                                <Server image={backgroundAeldra} serverName="Nayah II" price="100kkk Yang = 10 €" serverDescription="Nayah2 brings a balanced PvP experience to the Metin2 scene, offering many battlegrounds as well as some PvM content along the way."/>
+                                <Server image={backgroundMenor} serverName="Age of Menor" price="1.2kkk Yang = 10 €" serverDescription="There’s nothing on the market quite like Age of Menor. This International server brings back the old-school vibes that made you fall in love with Metin2 in the first place. Feeling nostalgic? Give this highly-populated server a try."/>
+                                <Server image={backgroundAeldra} serverName="Xaleas" price="150kk Yang = 10 €" serverDescription="Xaleas is a brand new server that nailed the perfect combination of oldschool and middleschool. With a nice progressive system focused on PvM, this server will surely make you want to stay."/>
                             </div>
                             <div className="row">
-                                <Server image={background6} serverName="Nexus II" price="2kkk Yang = 10 €" serverDescription="The definition of a PvM experience, this server brings a great deal of content to the table. An ever-growing community that actually makes its voice heard awaits for you!"/>
-                                <Server image={background5} serverName="Evion II" price="2.1kkk Yang = 10 €" serverDescription="Beginner-friendly and International, Evion2 is sure to please those of you who are looking for an adventure. Grab your friends and jump in this promising PvM experience together."/>
+                                <Server image={background6} serverName="Nexus II" price="3kkk Yang = 10 €" serverDescription="The definition of a PvM experience, this server brings a great deal of content to the table. An ever-growing community that actually makes its voice heard awaits for you!"/>
+                                <Server image={background5} serverName="Evion II" price="2.2kkk Yang = 10 €" serverDescription="Beginner-friendly and International, Evion2 is sure to please those of you who are looking for an adventure. Grab your friends and jump in this promising PvM experience together."/>
                             </div>
                             <div className="row">
                                 <Server image={background7} serverName="Anima" price="6 won/600kk = 10 €" serverDescription="The latest addition to the official DE metin2 server-list will surely offer you a great challenge with a fresh old-school vibe."/>
                                 <Server image={background8} serverName="Germania" price="10 won/1kkk = 10 €" serverDescription="As part of the most populated official server, the old Genesis community will make sure you have a great time and enjoy the experience."/>
+                            </div>
+                            <div className="row">
+                                <Server image={background9} serverName="Teuotonia" price="10 won/1kkk = 10 €" serverDescription="As any other official server, Teutonia takes the meaning of balanced and long lasting oldschool gaming experiences to the next level."/>
+                                <Server image={background10} serverName="Columba" price="10 won/1kkk = 10 €" serverDescription="After the recent server merge, Columba became the home of many communities united by a common goal. To have the most fun and grow together."/>
                             </div>
                             
                         </div>
