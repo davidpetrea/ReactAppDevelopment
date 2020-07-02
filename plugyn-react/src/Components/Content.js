dfb09d2799d5cae168a01488a4e02ab1859fd5f4
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2020-07-02 12:34:31 +0300

    price

--- plugyn-react/src/Components/Content.js
+++ plugyn-react/src/Components/Content.js
@@ -41,20 +41,20 @@ class Content extends React.Component{
                         </div>
                         <div className="container servers">
                             <div className="row">     
-                                 <Server image={backgroundCelestial} serverName="Rubinum" price="11kkk = 10 €" serverDescription="After a year long break, Rubinum, the genre defining private server, is finally back. There is no better time than now to join and climb the ladder of most promising private server out there at the moment."/>
+                                 <Server image={backgroundCelestial} serverName="Rubinum" price="15kkk = 10 €" serverDescription="After a year long break, Rubinum, the genre defining private server, is finally back. There is no better time than now to join and climb the ladder of most promising private server out there at the moment."/>
                             </div>
                             <div className="row">
-                                <Server image={backgroundEkstasia} serverName="Origins II" price="120kkk Yang = 10 €" serverDescription="With over 2000 players online, Origins2 has been a stable server for the past 8 months, enjoyable for those of you who are looking for a PvM – medium experience."/>
+                                <Server image={backgroundEkstasia} serverName="Origins II" price="110kkk Yang = 10 €" serverDescription="With over 2000 players online, Origins2 has been a stable server for the past 8 months, enjoyable for those of you who are looking for a PvM – medium experience."/>
                                 <Server image={background13} serverName="WoM II" price="1.5 kkk = 10 €" serverDescription="WoM II is one of the oldest private servers that combines a harder but more rewarding approach with stability and high standards. There will always be a place waiting for you on WoM II!"/>
 
                             </div>
                             <div  className="row">
                                 <Server image={backgroundMenor} serverName="Age of Menor" price="2kkk Yang = 10 €" serverDescription="There’s nothing on the market quite like Age of Menor. This International server brings back the old-school vibes that made you fall in love with Metin2 in the first place. Feeling nostalgic? Give this highly-populated server a try."/>
-                                <Server image={backgroundAeldra} serverName="Xaleas" price="5kkk Yang = 10 €" serverDescription="Xaleas is a brand new server that nailed the perfect combination of oldschool and middleschool. With a nice progressive system focused on PvM, this server will surely make you want to stay."/>
+                                <Server image={backgroundAeldra} serverName="Xaleas" price="6kkk Yang = 10 €" serverDescription="Xaleas is a brand new server that nailed the perfect combination of oldschool and middleschool. With a nice progressive system focused on PvM, this server will surely make you want to stay."/>
                             </div>
                             <div className="row">
                                 <Server image={background6} serverName="Celestial World II" price="70k Lagis = 10 €" serverDescription="This 6-year old new-school metin2 is still kicking. One of the most unique-rich servers on the market, with mechanics similar to those in Diablo and Path of Exile, is waiting for you."/>
-                                <Server image={background5} serverName="Evion II" price="5kkk Yang = 10 €" serverDescription="Beginner-friendly and International, Evion2 is sure to please those of you who are looking for an adventure. Grab your friends and jump in this promising PvM experience together."/>
+                                <Server image={background5} serverName="Evion II" price="10kkk Yang = 10 €" serverDescription="Beginner-friendly and International, Evion2 is sure to please those of you who are looking for an adventure. Grab your friends and jump in this promising PvM experience together."/>
                             </div>
                             <div className="row">
                                 <Server image={background7} serverName="Anima" price="7 won/700kk = 10 €" serverDescription="The latest addition to the official DE metin2 server-list will surely offer you a great challenge with a fresh old-school vibe."/>
