2befcbb1caaf522af5e766ae561f99d84c984a68
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2020-07-20 00:10:42 +0300

    price change

--- plugyn-react/src/Components/Content.js
+++ plugyn-react/src/Components/Content.js
@@ -42,12 +42,12 @@ class Content extends React.Component{
                         <div className="container servers">
                             
                             <div className="row">
-                                <Server image={backgroundCelestial} serverName="Rubinum" price="18kkk = 10 €" serverDescription="After a year long break, Rubinum, the genre defining private server, is finally back. There is no better time than now to join and climb the ladder of most promising private server out there at the moment."/>
-                                <Server image={backgroundEkstasia} serverName="Origins II" price="110kkk Yang = 10 €" serverDescription="With over 2000 players online, Origins2 has been a stable server for the past 8 months, enjoyable for those of you who are looking for a PvM – medium experience."/>
+                                <Server image={backgroundCelestial} serverName="Rubinum" price="19kkk = 10 €" serverDescription="After a year long break, Rubinum, the genre defining private server, is finally back. There is no better time than now to join and climb the ladder of most promising private server out there at the moment."/>
+                                <Server image={backgroundEkstasia} serverName="Origins II" price="100kkk Yang = 10 €" serverDescription="With over 2000 players online, Origins2 has been a stable server for the past 8 months, enjoyable for those of you who are looking for a PvM – medium experience."/>
                             </div>
                             <div  className="row">
                                 <Server image={backgroundMenor} serverName="Age of Menor" price="2kkk Yang = 10 €" serverDescription="There’s nothing on the market quite like Age of Menor. This International server brings back the old-school vibes that made you fall in love with Metin2 in the first place. Feeling nostalgic? Give this highly-populated server a try."/>
-                                <Server image={backgroundAeldra} serverName="Xaleas" price="6kkk Yang = 10 €" serverDescription="Xaleas is a brand new server that nailed the perfect combination of oldschool and middleschool. With a nice progressive system focused on PvM, this server will surely make you want to stay."/>
+                                <Server image={backgroundAeldra} serverName="Xaleas" price="10kkk Yang = 10 €" serverDescription="Xaleas is a brand new server that nailed the perfect combination of oldschool and middleschool. With a nice progressive system focused on PvM, this server will surely make you want to stay."/>
                             </div>
                             <div className="row">
                                 <Server image={background6} serverName="Celestial World II" price="70k Lagis = 10 €" serverDescription="This 6-year old new-school metin2 is still kicking. One of the most unique-rich servers on the market, with mechanics similar to those in Diablo and Path of Exile, is waiting for you."/>
@@ -55,15 +55,15 @@ class Content extends React.Component{
                             </div>
                             <div className="row">
                                 <Server image={background7} serverName="Anima" price="7 won/700kk = 10 €" serverDescription="The latest addition to the official DE metin2 server-list will surely offer you a great challenge with a fresh old-school vibe."/>
-                                <Server image={background8} serverName="Germania" price="1.2 € = 1 won" serverDescription="As part of the most populated official server, the old Genesis community will make sure you have a great time and enjoy the experience."/>
+                                <Server image={background8} serverName="Germania" price="1.1 € = 1 won" serverDescription="As part of the most populated official server, the old Genesis community will make sure you have a great time and enjoy the experience."/>
                             </div>
                             <div className="row">
-                                <Server image={background9} serverName="Teutonia" price="1.2 € = 1 won" serverDescription="As any other official server, Teutonia takes the meaning of balanced and long lasting oldschool gaming experiences to the next level."/>
+                                <Server image={background9} serverName="Teutonia" price="1.1 € = 1 won" serverDescription="As any other official server, Teutonia takes the meaning of balanced and long lasting oldschool gaming experiences to the next level."/>
                                 <Server image={background10} serverName="Columba" price="11 won/1.1kkk = 10 €" serverDescription="After the recent server merge, Columba became the home of many communities united by a common goal. To have the most fun and grow together."/>
                             </div>
                             <div className="row">
-                                <Server image={background11} serverName="Communio" price="110 kk = 10 €" serverDescription="Communio represents exactly what is says: a place for everyone, a fresh start where new adventurers can join and compete in the long run."/>
-                                <Server image={background12} serverName="Sodalitas" price="80 kk = 10 €" serverDescription="Sodalitas is the clearest example of what the Metin2 community needs: high-quality, stable experiences of the oldschool grind that everyone is nostalgic for. "/>
+                                <Server image={background11} serverName="Communio" price="150 kk = 10 €" serverDescription="Communio represents exactly what is says: a place for everyone, a fresh start where new adventurers can join and compete in the long run."/>
+                                <Server image={background12} serverName="Sodalitas" price="100 kk = 10 €" serverDescription="Sodalitas is the clearest example of what the Metin2 community needs: high-quality, stable experiences of the oldschool grind that everyone is nostalgic for. "/>
                             </div>
                             
                             
